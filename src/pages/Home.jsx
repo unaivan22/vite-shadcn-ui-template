c4f1c899596cd315f2cffd0d3a73d4036d84a99b
unaivan22 <unaivan22@gmail.com>
2025-03-04 09:45:56 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,7 @@ export default function Home() {
     console.log('home loaded')
   return (
     <div>
-      Welcome home una sadasdasd
-
-      dsadsadsa
+      Hallo, vite 5.1.4 dan tailwind v4
     </div>
   )
 }
